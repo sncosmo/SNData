a50b1fb063724d424fc3741881cfde477b257376
djperrefort <djperrefort@pitt.edu>
2019-06-28 12:11:29 -0400

    Moves sncosmo to install requires

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(name='SNData',
       license='GPL v3',
 
       python_requires='>=3.6',
-      install_requires=requirements,
+      install_requires=requirements + ['sncosmo'],
 
       setup_requires=['pytest-runner'],
       tests_require=['pytest'],
