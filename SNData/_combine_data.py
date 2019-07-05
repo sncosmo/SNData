1349aed2367c496b46855dd869b11c50007dd032
djperrefort <djperrefort@pitt.edu>
2019-07-05 14:44:49 -0400

    Adds ability to join ID values

--- SNData/_combine_data.py
+++ SNData/_combine_data.py
@@ -3,21 +3,63 @@
 
 """This module handles combining data from different data sets."""
 
+from copy import copy
+
 import pandas as pd
+from astropy.table import vstack
 
 from . import _utils as utils
 
 
+def _reduce_id_mapping(id_list):
+    """Combine a list of sets by unioning any sets with shared elements
+
+    Args
+        id_list (list[tuple[str]]): List of object IDs to join
+
+    Returns:
+        A list of combined sets
+    """
+
+    old_id_list = copy(id_list)
+    new_id_list = []
+
+    while old_id_list:
+        first, *rest = old_id_list
+
+        lf = -1
+        while len(first) > lf:
+            lf = len(first)
+            rest2 = []
+            for r in rest:
+                if len(first.intersection(r)) > 0:
+                    first |= r
+
+                else:
+                    rest2.append(r)
+
+            rest = rest2
+
+        new_id_list.append(first)
+        old_id_list = rest
+
+    return new_id_list
+
+
 # Todo: Class Docstring
-# Todo: Implement ID joining
 class CombinedDataset:
+    """Combine data from different surveys into a single data set
+
+    Args:
+        data_sets (iter[module]): SNData module for a specific data release
+    """
 
     def __init__(self, *data_sets):
-        """Combine data from different surveys into a single data set"""
 
+        # Data access modules for each combined data release
         self._data_modules = dict()
 
-        # Create a DataFrame of combined object ids
+        # Create a DataFrame of combined object IDs
         self._obj_ids = None
         for data_module in set(data_sets):
             _, survey, release = data_module.__name__.split('.')
@@ -32,6 +74,8 @@ class CombinedDataset:
 
             else:
                 self._obj_ids.append(id_df, ignore_index=True)
+
+        self._joined_ids = []
 
     def download_module_data(self, force=False):
         """Download data for all combined surveys / data releases
@@ -51,7 +95,7 @@ class CombinedDataset:
             module.delete_module_data()
 
     def get_available_ids(self):
-        """Return a table of object ids available in the combined data set"""
+        """Return a table of object IDs available in the combined data set"""
 
         data_order = ['obj_id', 'release', 'survey']
         return sorted(
@@ -59,14 +103,14 @@ class CombinedDataset:
         )
 
     def get_duplicate_ids(self, ignore_survey=True, ignore_release=True):
-        """Return a table of duplicate object ids for the combined data set
+        """Return a table of duplicate object IDs for the combined data set
 
         Args:
-            ignore_survey  (bool): Don't report ids from different surveys (Default: True)
-            ignore_release (bool): Don't report ids from different releases (Default: True)
+            ignore_survey  (bool): Don't report IDs from different surveys (Default: True)
+            ignore_release (bool): Don't report IDs from different releases (Default: True)
 
         Returns:
-            An astropy table of duplicate id values
+            An astropy table of duplicate ID values
         """
 
         subset = ['obj_id']
@@ -95,13 +139,11 @@ class CombinedDataset:
                 raise utils.NoDownloadedData(
                     f'No data downloaded for {module.__name__}')
 
-    def get_data_for_id(self, obj_id, format_sncosmo=False):
-        """Return data for a given object id
-
-        See ``get_available_ids()`` for a table of available id values. Object
+    def _get_data_single_id(self, obj_id, format_sncosmo=False):
+        """Return data for a given object ID
 
         Args:
-            obj_id   (Tuple[str]): The ID of the desired object
+            obj_id   (tuple[str]): The ID of the desired object
             format_sncosmo (bool): Format data for SNCosmo.fit_lc (Default: False)
 
         Returns:
@@ -112,15 +154,64 @@ class CombinedDataset:
             (self._obj_ids['obj_id'] == obj_id[0]) &
             (self._obj_ids['release'] == obj_id[1]) &
             (self._obj_ids['survey'] == obj_id[2])
-        ]
+            ]
 
         if len(id_data) == 0:
             raise ValueError('Unrecognized object ID')
 
-        module_key = f"{id_data['survey'].iloc[0]}:{id_data['release'].iloc[0]}"
+        module_key = \
+            f"{id_data['survey'].iloc[0]}:{id_data['release'].iloc[0]}"
+
         data_module = self._data_modules[module_key]
         return data_module.get_data_for_id(
             obj_id[0], format_sncosmo=format_sncosmo)
+
+    def _get_data_id_list(self, obj_id_list, format_sncosmo=False):
+        """Return data for a list of object ID
+
+        Data tables for individual object IDs are vertically stacked. Meta
+        data for each individual obj_id is stored in the combined
+
+        Args:
+            obj_id_list (list[tuple[str]]): The ID of the desired object
+            format_sncosmo          (bool): Format data for SNCosmo.fit_lc (Default: False)
+
+        Returns:
+            An astropy table of data for the given ID
+        """
+
+        combined_table = None
+        for obj_id in obj_id_list:
+            data_table = self._get_data_single_id(obj_id, format_sncosmo)
+
+            if combined_table is None:
+                data_table.meta['obj_id'] = [data_table.meta['obj_id']]
+                data_table.meta[obj_id] = data_table.meta
+                combined_table = data_table
+
+            else:
+                combined_table.meta['obj_id'].append(data_table.meta['obj_id'])
+                combined_table.meta[obj_id] = data_table.meta
+                combined_table = vstack((combined_table, data_table))
+
+    def get_data_for_id(self, obj_id, format_sncosmo=False):
+        """Return data for a given object ID
+
+        See ``get_available_ids()`` for a table of available ID values. Object
+
+        Args:
+            obj_id   (tuple[str]): The ID of the desired object
+            format_sncosmo (bool): Format data for SNCosmo.fit_lc (Default: False)
+
+        Returns:
+            An astropy table of data for the given ID
+        """
+
+        for id_set in self._joined_ids:
+            if obj_id in id_set:
+                return self._get_data_id_list(obj_id, format_sncosmo)
+
+        return self.get_data_for_id(obj_id, format_sncosmo)
 
     def iter_data(self, survey=None, release=None, verbose=False,
                   format_sncosmo=False, filter_func=None):
@@ -158,10 +249,33 @@ class CombinedDataset:
                 yield data
 
     def get_joined_ids(self):
-        pass
+        """Return a list of joined object IDs
 
-    def join_ids(self, *obj_ids):
-        pass
+        Return:
+            A list of joined object IDs [{id_1, id_2}, ...]
+        """
 
-    def seperate_ids(self, mapping):
-        pass
+        return copy(self._joined_ids)
+
+    def join_ids(self, obj_ids):
+        """Join a list of object ID values to indicate the same object
+
+        Args:
+            obj_ids (list[tuple[str]]): List of object IDs to join
+        """
+
+        self._joined_ids.append(set(obj_ids))
+        self._joined_ids = _reduce_id_mapping(self._joined_ids)
+
+    def separate_ids(self, obj_ids):
+        """Separate a list of object IDs so they are no longer joined to other IDs
+
+        Args:
+            obj_ids (list[tuple[str]]): List of object IDs to separate
+        """
+
+        obj_ids = set(obj_ids)
+        for obj_id_set in self._joined_ids:
+            obj_id_set -= obj_ids
+
+        self._joined_ids = _reduce_id_mapping(self._joined_ids)
