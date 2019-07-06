c7e031cca2c12f504e2ccbdd3deb10aa1609b340
djperrefort <djperrefort@pitt.edu>
2019-07-06 14:13:00 -0400

    Version bump [ci skip]

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,9 +24,9 @@ copyright = '2019, Daniel Perrefort'
 author = 'Daniel Perrefort'
 
 # The short X.Y version
-version = '0.3.1'
+version = '0.3.3'
 # The full version, including alpha/beta/rc tags
-release = '0.3.1'
+release = '0.3.3'
 
 # -- General configuration ---------------------------------------------------
 
