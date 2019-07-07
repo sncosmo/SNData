14761ee8223282303b90ded484d3a5df96eb3577
djperrefort <djperrefort@pitt.edu>
2019-07-07 16:52:03 -0400

    Outlines additional tests for CombinedDataset class (fails)

--- tests/combined_datasets.py
+++ tests/combined_datasets.py
@@ -7,18 +7,46 @@ sets
 
 from unittest import TestCase
 
+from astropy.table import vstack
+
 from SNData import csp, des
 from SNData._combine_data import CombinedDataset, _reduce_id_mapping
 
 
-# Todo: Test CombinedDataset class
+# Todo: Finish tests for CombinedDataset class
 class Combined(TestCase):
-    """Tests the combined des.sn3yr and csp.dr3 modules"""
+    """Tests the CombinedDataset class using des.sn3yr and csp.dr3 data"""
 
     @classmethod
     def setUpClass(cls):
         cls.module = CombinedDataset(csp.dr3, des.sn3yr)
         cls.module.download_module_data()
+
+    def test_id_joining(self):
+        """Test correct data is returned after joining / separating IDs"""
+
+        test_ids = [('csp', 'dr3', '2004dt'), ('csp', 'dr3', '2004ef')]
+        obj1_data = self.module.get_data_for_id(test_ids[0], True)
+        obj2_data = self.module.get_data_for_id(test_ids[1], True)
+        expected_return = vstack(obj1_data, obj2_data)
+
+        self.module.join_ids(*test_ids)
+        actual_return = self.module.get_data_for_id(test_ids[0])
+        self.assertEqual(expected_return, actual_return)
+
+        self.module.separate_ids(test_ids[1])
+        self.assertEqual(self.module.get_data_for_id(test_ids[0]), obj1_data)
+
+    def test_iter_data(self):
+        """Test ``iter_data`` correctly slices data by survey / release value
+        """
+
+        pass
+
+    def test_duplicate_ids(self):
+        """Test ``get_duplicate_ids`` returns the correct values"""
+
+        pass
 
 
 class MapReduction(TestCase):
