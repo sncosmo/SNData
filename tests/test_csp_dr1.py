5fd1eaa330b00b9d3d228e37f5aca759aa041c65
djperrefort <djperrefort@pitt.edu>
2019-09-20 10:09:57 -0400

    Updates tests

--- tests/test_csp_dr1.py
+++ tests/test_csp_dr1.py
@@ -24,9 +24,6 @@ class DataParsing(DataParsingTestBase):
     def test_ids_are_sorted(self):
         self._test_ids_are_sorted()
 
-    def test_format_sncosmo_raises_err(self):
-        self._test_format_sncosmo_raises_err()
-
 
 class Documentation(DocumentationTestBase):
     """Tests for the des.SN3YR module"""
