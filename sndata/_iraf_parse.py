0b207b8236cdeb1cff519c192c952162c55330f2
djperrefort <djperrefort@pitt.edu>
2019-11-13 09:45:41 -0500

    Formatting and f-strings

--- sndata/_iraf_parse.py
+++ sndata/_iraf_parse.py
@@ -14,7 +14,7 @@ import numpy as np
 from astropy.io import fits as pyfits
 
 
-def nonlinearwave(nwave, specstr, verbose=False):
+def nonlinear_wave(nwave, specstr, verbose=False):
     """Compute non-linear wavelengths from multispec string
 
     Args:
@@ -26,39 +26,33 @@ def nonlinearwave(nwave, specstr, verbose=False):
         An array of wavelength values
         The dispersion fields
     """
-    print('f', specstr)
 
     fields = specstr.split()
     if int(fields[2]) != 2:
-        raise ValueError('Not nonlinear dispersion: dtype=' + fields[2])
+        raise ValueError(f'Not nonlinear dispersion: dtype={fields[2]}')
 
     if len(fields) < 12:
-        raise ValueError('Bad spectrum format (only %d fields)' % len(fields))
+        raise ValueError(f'Bad spectrum format (only {len(fields)} fields)')
 
     ftype = int(fields[11])
-    if ftype == 3:
-
-        # cubic spline
-
+    if ftype == 3:  # Cubic spline
         if len(fields) < 15:
-            raise ValueError(
-                'Bad spline format (only %d fields)' % len(fields))
+            raise ValueError(f'Bad spline format (only {len(fields)} fields)')
 
         npieces = int(fields[12])
         pmin = float(fields[13])
         pmax = float(fields[14])
-
         if verbose:
-            print('Dispersion is order-%d cubic spline' % npieces)
+            print(f'Dispersion is order-{npieces} cubic spline')
 
         if len(fields) != 15 + npieces + 3:
-            raise ValueError('Bad order-%d spline format (%d fields)' % (
-                npieces, len(fields)))
+            raise ValueError(
+                f'Bad order-{npieces} spline format ({len(fields)} fields)')
 
-        coeff = np.asarray(fields[15:], dtype=float)
-        # normalized x coordinates
-        s = (np.arange(nwave, dtype=float) + 1 - pmin) / (
-                pmax - pmin) * npieces
+        coefficients = np.asarray(fields[15:], dtype=float)
+
+        # Normalized x coordinates
+        s = (np.arange(nwave, dtype=float) + 1 - pmin) / (pmax - pmin) * npieces
         j = s.astype(int).clip(0, npieces - 1)
         a = (j + 1) - s
         b = s - j
@@ -66,39 +60,39 @@ def nonlinearwave(nwave, specstr, verbose=False):
         x1 = 1 + 3 * a * (1 + a * b)
         x2 = 1 + 3 * b * (1 + a * b)
         x3 = b ** 3
-        wave = coeff[j] * x0 + coeff[j + 1] * x1 + coeff[j + 2] * x2 + coeff[
-            j + 3] * x3
+        wave = (
+                coefficients[j] * x0 +
+                coefficients[j + 1] * x1 +
+                coefficients[j + 2] * x2 +
+                coefficients[j + 3] * x3
+        )
 
     elif ftype == 1 or ftype == 2:
-
         # chebyshev or legendre polynomial
         # legendre not tested yet
         if len(fields) < 15:
             raise ValueError(
-                'Bad polynomial format (only %d fields)' % len(fields))
+                f'Bad polynomial format (only {len(fields)} fields)')
 
         order = int(fields[12])
         pmin = float(fields[13])
         pmax = float(fields[14])
         if verbose:
             if ftype == 1:
-                print('Dispersion is order-%d Chebyshev polynomial' % order)
+                print(f'Dispersion is order-{order} Chebyshev polynomial')
 
             else:
-                print(
-                    'Dispersion is order-%d Legendre polynomial (NEEDS TEST)' % order)
+                print(f'Dispersion is order-{order} Legendre polynomial (NEEDS TEST)')
 
         if len(fields) != 15 + order:
             # raise ValueError('Bad order-%d polynomial format (%d fields)' % (order, len(fields)))
             if verbose:
-                print('Bad order-%d polynomial format (%d fields)' % (
-                    order, len(fields)))
-                print(
-                    "Changing order from %i to %i" % (order, len(fields) - 15))
+                print(f'Bad order-{order} polynomial format ({len(fields)} fields)')
+                print(f'Changing order from {order} to {len(fields) - 15}')
 
             order = len(fields) - 15
 
-        coeff = np.asarray(fields[15:], dtype=float)
+        coefficients = np.asarray(fields[15:], dtype=float)
 
         # normalized x coordinates
         pmiddle = (pmax + pmin) / 2
@@ -106,7 +100,7 @@ def nonlinearwave(nwave, specstr, verbose=False):
         x = (np.arange(nwave, dtype=float) + 1 - pmiddle) / (prange / 2)
         p0 = np.ones(nwave, dtype=float)
         p1 = x
-        wave = p0 * coeff[0] + p1 * coeff[1]
+        wave = p0 * coefficients[0] + p1 * coefficients[1]
 
         for i in range(2, order):
             if ftype == 1:  # chebyshev
@@ -115,18 +109,17 @@ def nonlinearwave(nwave, specstr, verbose=False):
             else:  # legendre
                 p2 = ((2 * i - 1) * x * p1 - (i - 1) * p0) / i
 
-            wave = wave + p2 * coeff[i]
+            wave = wave + p2 * coefficients[i]
             p0 = p1
             p1 = p2
 
     else:
-        raise ValueError(
-            'Cannot handle dispersion function of type %d' % ftype)
+        raise ValueError(f'Cannot handle dispersion function of type {ftype}')
 
     return wave, fields
 
 
-def read_multispec(fitsfile, reform=True, quiet=False):
+def read_multispec(fitsfile, reform=True, quiet=True):
     """Read an IRAF spectrum in multispec format from a FITS file
 
     Args:
@@ -160,12 +153,15 @@ def read_multispec(fitsfile, reform=True, quiet=False):
     try:
         header = fh[0].header
         flux = fh[0].data
+
     finally:
         fh.close()
+
     temp = flux.shape
     nwave = temp[-1]
     if len(temp) == 1:
         nspec = 1
+
     else:
         nspec = temp[-2]
 
@@ -178,8 +174,10 @@ def read_multispec(fitsfile, reform=True, quiet=False):
         if ctype1.strip() == 'LINEAR':
             wavelen = np.zeros((nspec, nwave), dtype=float)
             ww = (np.arange(nwave, dtype=float) + 1 - crpix1) * cd1_1 + crval1
+
             for i in range(nspec):
                 wavelen[i, :] = ww
+
             # handle log spacing too
             dcflag = header.get('dc-flag', 0)
             if dcflag == 1:
@@ -200,8 +198,7 @@ def read_multispec(fitsfile, reform=True, quiet=False):
                 flux = np.squeeze(flux)
                 wavelen.shape = (nwave,)
 
-            return {'flux': flux, 'wavelen': wavelen, 'header': header,
-                    'wavefields': None}
+            return {'flux': flux, 'wavelen': wavelen, 'header': header, 'wavefields': None}
     except KeyError:
         pass
 
@@ -223,6 +220,7 @@ def read_multispec(fitsfile, reform=True, quiet=False):
         v = wat2[i]
         v = v + (" " * (68 - len(v)))  # restore trailing blanks
         watstr.append(v)
+
     watstr = ''.join(watstr)
 
     # find all the spec#="..." strings
@@ -233,7 +231,8 @@ def read_multispec(fitsfile, reform=True, quiet=False):
         p2 = watstr.find('"', p1)
         p3 = watstr.find('"', p2 + 1)
         if p1 < 0 or p1 < 0 or p3 < 0:
-            raise ValueError('Cannot find ' + sname + ' in WAT2_* keyword')
+            raise ValueError('Cannot find {sname} in WAT2_* keyword')
+
         specstr[i] = watstr[p2 + 1:p3]
 
     wparms = np.zeros((nspec, 9), dtype=float)
@@ -242,8 +241,7 @@ def read_multispec(fitsfile, reform=True, quiet=False):
         wparms[i, :] = w1[:9]
         if w1[2] == -1:
             raise ValueError(
-                'Spectrum %d has no wavelength calibration (type=%d)' % (
-                    i + 1, w1[2]))
+                f'Spectrum {i + 1} has no wavelength calibration (type={w1[2]})')
 
             # elif w1[6] != 0:
             #    raise ValueError('Spectrum %d has non-zero redshift (z=%f)' % (i+1,w1[6]))
@@ -256,8 +254,7 @@ def read_multispec(fitsfile, reform=True, quiet=False):
         verbose = (not quiet) and (i == 0)
         if wparms[i, 2] == 0 or wparms[i, 2] == 1:
             # simple linear or log spacing
-            wavelen[i, :] = np.arange(nwave, dtype=float) * wparms[i, 4] + \
-                            wparms[i, 3]
+            wavelen[i, :] = np.arange(nwave, dtype=float) * wparms[i, 4] + wparms[i, 3]
             if wparms[i, 2] == 1:
                 wavelen[i, :] = 10.0 ** wavelen[i, :]
                 if verbose:
@@ -268,17 +265,19 @@ def read_multispec(fitsfile, reform=True, quiet=False):
 
         else:
             # non-linear wavelengths
-            wavelen[i, :], wavefields[i] = nonlinearwave(
+            wavelen[i, :], wavefields[i] = nonlinear_wave(
                 nwave, specstr[i], verbose=verbose)
 
         wavelen *= 1.0 + wparms[i, 6]
         if verbose:
-            print("Correcting for redshift: z=%f" % wparms[i, 6])
+            print(f"Correcting for redshift: z={wparms[i, 6]}")
 
     if nspec == 1 and reform:
         # get rid of unity dimensions
         flux = np.squeeze(flux)
         wavelen.shape = (nwave,)
 
-    return {'flux': flux, 'wavelen': wavelen, 'header': header,
+    return {'flux': flux,
+            'wavelen': wavelen,
+            'header': header,
             'wavefields': wavefields}
