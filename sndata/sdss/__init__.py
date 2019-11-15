e5efb40cf96bc8f962eca0ffe28bea79390aa87c
djperrefort <djperrefort@pitt.edu>
2019-11-14 21:51:39 -0500

    Updates sdss docstring

--- sndata/sdss/__init__.py
+++ sndata/sdss/__init__.py
@@ -2,8 +2,9 @@
 # -*- coding: UTF-8 -*-
 
 """The ``sdss`` module provides access to supernova data from the Sloan
-Digital Sky Survey (SDSS). For more information on SDSS see
-https://www.sdss.org .
+Digital Sky Survey (SDSS). Please note that access to the photometric and
+spectroscopic components of the Sako et al. 2018 data release is provided
+by separate modules. For more information on SDSS see https://www.sdss.org .
 """
 
 from . import sako18, sako18spec
