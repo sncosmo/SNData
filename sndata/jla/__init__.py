9a74022ab6c7f004ec828b6c6eec8c934a83f910
djperrefort <djperrefort@pitt.edu>
2020-01-24 14:44:51 -0700

    Updates docs

--- sndata/jla/__init__.py
+++ sndata/jla/__init__.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3.7
 # -*- coding: UTF-8 -*-
 
-""""""
+"""The ``jla`` module provides access to supernova data compiled by the
+Joint Light-Curve Analysis project. It includes recalibrated light-curves
+of type Ia supernova (SN Ia) from the SDSS-II and SNLS collaborations.
+"""
 
 from . import betoule14
 
