e4f39f04accb6996154a210d1ab1c55f50da18c6
djperrefort <djperrefort@pitt.edu>
2020-02-20 15:17:15 -0500

    Adds more tests for utils

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 # -*- coding: UTF-8 -*-
 
+from pathlib import Path
 from unittest import TestCase
 
 import numpy as np
 
 from sndata import _utils as utils
+from sndata.exceptions import NoDownloadedData
 
 
 class ConvertToJD(TestCase):
@@ -40,3 +42,19 @@ class ConvertToJD(TestCase):
         self.assertEqual(
             self.expected_jd, utils.convert_to_jd(self.jd_date),
             'Incorrect date for JD format')
+
+
+class CheckUrl(TestCase):
+    """Tests for the ``convert_to_jd`` function"""
+
+    def test_offline_url_warning(self):
+        fake_url = 'https://www.thisurllisfake.com/'
+        self.assertWarns(Warning, utils.check_url, fake_url)
+
+
+class RequireDataPath(TestCase):
+    """Tests for the ``require_data_path`` function"""
+
+    def test_missing_dir_raises_error(self):
+        fake_dir = Path('./This_dir_is_fake')
+        self.assertRaises(NoDownloadedData, utils.require_data_path, fake_dir)
