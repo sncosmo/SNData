e44eab37170f7d9806cb8df4d21ad7311c99c40b
Daniel Perrefort <djperrefort@pitt.edu>
2020-02-24 08:53:59 -0500

    Minor formatting for custom exceptions

--- sndata/exceptions.py
+++ sndata/exceptions.py
@@ -6,33 +6,23 @@
 
 class NoDownloadedData(Exception):
     def __init__(self, *args, **kwargs):
-        default_message = \
-            'Data has not been downloaded for this module / data release.'
-
-        if not (args or kwargs):
-            args = (default_message,)
-
+        default_message = 'Data has not been downloaded for this data release.'
+        args = args if args else (default_message,)
         super().__init__(*args, **kwargs)
 
 
 class InvalidObjId(Exception):
     def __init__(self, *args, **kwargs):
-        default_message = 'The provided object Id is not valid.'
-
-        if not (args or kwargs):
-            args = (default_message,)
-
+        args = args if args else ('The provided object Id is not valid.',)
         super().__init__(*args, **kwargs)
 
 
 class ObservedDataTypeError(Exception):
     def __init__(self, *args, **kwargs):
+        # Add default message
         default_message = (
             'This action is not valid for the type of data '
-            'included in the current data release.'
+            'included in the current data release.',
         )
-
-        if not (args or kwargs):
-            args = (default_message,)
-
+        args = args if args else (default_message,)
         super().__init__(*args, **kwargs)
