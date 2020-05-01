03cf5a16ea8388bb07d9ab586de8d212ea304bdf
djperrefort <djperrefort@pitt.edu>
2020-04-30 22:12:15 -0400

    Adds inital Stahl20 module

---
+++ sndata/bsnip/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""The ``bsnip`` module provides access to data from the Berkeley Supernova
+Ia Program (BSNIP). For the photometric compliment to this survey, see the
+``loss`` module.
+"""
+
+from ._stahl20 import Stahl20
+
+survey_name = 'Berkeley Supernova Ia Program'
+survey_abbrev = 'BSNIP'
--- sndata/loss/__init__.py
+++ sndata/loss/__init__.py
@@ -2,11 +2,11 @@
 # -*- coding: UTF-8 -*-
 
 """The ``loss`` module provides access to supernova data from the Lick
-Observatory Supernova Search (LOSS).
+Observatory Supernova Search (LOSS). For the spectroscopic compliment to this
+survey, see the ``bsnip`` module.
 """
 
 from ._stahl19 import Stahl19
-
 
 survey_name = 'Lick Observatory Supernova Search'
 survey_abbrev = 'LOSS'
