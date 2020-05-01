8469ddfe23c45406a9282ccd1578bac0f4f637b5
djperrefort <djperrefort@pitt.edu>
2020-05-01 14:56:47 -0400

    Initial sweetspot commit

---
+++ sndata/sweetspot/__init__.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: UTF-8 -*-
+
+"""The ``sweetspot`` module provides access to data from the Sweetspot survey"""
+
+from ._dr1 import DR1
+
+survey_name = 'Sweetspot'
+survey_abbrev = 'sweetspot'
