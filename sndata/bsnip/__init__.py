38eee430ac6bc2a3b73c485657afc2417f67ac39
Daniel <djperrefort@gmail.com>
2020-06-25 18:26:56 -0400

    Removes LOSS reference from BSNIP docs

--- sndata/bsnip/__init__.py
+++ sndata/bsnip/__init__.py
@@ -2,8 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 """The ``bsnip`` module provides access to data from the Berkeley Supernova
-Ia Program (BSNIP). For the photometric compliment to this survey, see the
-``loss`` module.
+Ia Program (BSNIP).
 """
 
 from ._stahl20 import Stahl20
