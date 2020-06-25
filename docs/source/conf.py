212edb76e6fdbbbc40b3c74f6e71c270f167da4e
Daniel <djperrefort@gmail.com>
2020-06-25 15:01:32 -0400

    Updates version in docs

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,7 +26,7 @@ copyright = '2019, Daniel Perrefort'
 author = 'Daniel Perrefort'
 
 # The short X.Y version
-version = '1.0.0'
+version = '1.1.1'
 
 # The full version, including alpha/beta/rc tags
 release = version
