bdcf68ea1214c4be3c30d50d220b22fe253baf03
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-28 06:46:47 +0000

    Adding tarfile member sanitization to extractall()

--- sndata/sdss/_sako18.py
+++ sndata/sdss/_sako18.py
@@ -266,7 +266,29 @@ class Sako18(PhotometricRelease, DefaultParser):
         outlier_archive = self._snana_dir / 'SDSS_allCandidates+BOSS.tar.gz'
         if outlier_archive.exists():
             with tarfile.open(str(outlier_archive), mode='r:gz') as data:
-                data.extractall(str(outlier_archive.parent))
+                
+                import os
+                
+                def is_within_directory(directory, target):
+                    
+                    abs_directory = os.path.abspath(directory)
+                    abs_target = os.path.abspath(target)
+                
+                    prefix = os.path.commonprefix([abs_directory, abs_target])
+                    
+                    return prefix == abs_directory
+                
+                def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                
+                    for member in tar.getmembers():
+                        member_path = os.path.join(path, member.name)
+                        if not is_within_directory(path, member_path):
+                            raise Exception("Attempted Path Traversal in Tar File")
+                
+                    tar.extractall(path, members, numeric_owner=numeric_owner) 
+                    
+                
+                safe_extract(data, str(outlier_archive.parent))
 
         for file_name in self._table_names:
             downloads.download_file(
