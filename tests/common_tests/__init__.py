e400afcf8efb9ce7d797b0048291eaf41fd063f7
Daniel <djperrefort@gmail.com>
2023-08-30 16:05:00 -0400

    Fixes bug in handling of connection errors

--- tests/common_tests/__init__.py
+++ tests/common_tests/__init__.py
@@ -1,17 +1,20 @@
 """This module defines reusable setup and testing logic for streamlining the
 creation of downstream tests.
 """
+
 from unittest import SkipTest
+
+import requests
 
 from .data_parsing_tests import *
 from .ui_tests import *
 
 
-def download_data_or_skip(release) -> None:
+def download_data_or_skip(release):
     """Download data for the given data release. Skip any further tests if the download fails"""
 
     try:
         release.download_module_data()
 
-    except ConnectionError:
+    except requests.exceptions.ConnectionError:
         raise SkipTest('Could not connect to one or more remote servers.')
