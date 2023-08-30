9af8c85f5d13e164e971132d3549948ac705a430
Daniel <djperrefort@gmail.com>
2023-08-30 16:44:56 -0400

    Revert attribute rename

--- sndata/base_classes.py
+++ sndata/base_classes.py
@@ -116,7 +116,7 @@ class SpectroscopicRelease(Base, metaclass=abc.ABCMeta):
         self.release = release if release else self.release
 
         self._data_dir = data_parsing.find_data_dir(self.survey_abbrev, self.release)
-        self._tables_dir = self._data_dir / 'tables'
+        self._table_dir = self._data_dir / 'tables'
 
     def get_available_tables(self) -> List[VizierTableId]:
         """Get Ids for available vizier tables published by this data release"""
